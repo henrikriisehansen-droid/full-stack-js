b0ec85ee706dc0044af879f742e7fa54325f85c0
Henrik Riise Hansen <51399211+henrikriisehansen@users.noreply.github.com>
2024-02-13 14:17:23 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello nodemon')
+})
+
+app.get('/api/courses',(req,res) =>{
+
+    res.send([1,2,3])
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
